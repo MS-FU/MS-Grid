cf5a5f39ec2130691a07881c7404f4293fd254c0
Daniel Daza <fredy.urrego@morningstar.com>
2016-06-17 14:35:52 -0500

    Add mousewheel event

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var jQuery = require("jquery");
+require("jquery-mousewheel")(jQuery);
 require("jquery.event.drag")(jQuery);
 require("jquery-ui");
-//require("jquery-mousewheel")(jQuery);
 var Slick = require("./slick.core.js")(jQuery);
 
 module.exports = jQuery.extend(Slick, {
